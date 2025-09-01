e6192e9a0170208a19487e0780f62363050bb024
Mkp991203 <manikumarkuyyeti@gmail.com>
2025-09-01 14:51:06 +0530

    wallet

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createWalletTransaction, getUserWallet } = require('../controllers/walletController');
+const { createWalletTransaction, getUserWallet, updateWalletTransaction } = require('../controllers/walletController');
 const router = express.Router();
 
 router.post('/createWalletTransaction', createWalletTransaction);
 router.get('/:customerID', getUserWallet);
+router.post('/updateWalletTransaction', updateWalletTransaction);
 
 
 module.exports = router;
